83c410932e76fa790ef1f197e83c623645a47797
AbdulRehman2236 <abdulrehmanfayyaz361@gmail.com>
2024-09-09 18:23:44 +0500

    Added header and sidebar

---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const Body = () => {
+  return (
+    <div className="p-3 px-6">
+      <Header />
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Body;
