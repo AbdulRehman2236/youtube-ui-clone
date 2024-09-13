31f42246128cd43989f809acee9f2c84661fff50
AbdulRehman2236 <abdulrehmanfayyaz361@gmail.com>
2024-09-13 14:37:02 +0500

    Added like dislike functionality

--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/slices/sidebarSlice";
@@ -9,6 +9,8 @@ import { getFormattedCount } from "../utils/helpers";
 const WatchVideo = () => {
   const [videoId] = useSearchParams();
   const dispatch = useDispatch();
+  const [isVideoLike, setIsVideoLike] = useState(false);
+  const [isVideoDislike, setIsVideoDislike] = useState(false);
   const videoDetails = useGetVideoDetails(videoId.get("v"));
 
   useEffect(() => {
@@ -16,8 +18,21 @@ const WatchVideo = () => {
   }, []);
 
   if (videoDetails.length === 0) return;
-  const { snippet, statistics, contentDetails } = videoDetails[0];
-  const { title, thumbnails, channelTitle, publishedAt } = snippet;
+  const { snippet, statistics } = videoDetails[0];
+  const { title, channelTitle, publishedAt } = snippet;
+
+  let numberOfLikes = getFormattedCount(statistics.likeCount);
+
+  const handleIsVideoLike = () => {
+    setIsVideoLike(!isVideoLike);
+    setIsVideoDislike(false);
+    isVideoLike ? (numberOfLikes += 1) : (numberOfLikes -= 1);
+  };
+
+  const handleIsVideoDislike = () => {
+    setIsVideoDislike(true);
+    setIsVideoLike(false);
+  };
 
   return (
     <>
@@ -59,10 +74,10 @@ const WatchVideo = () => {
         <div className="flex w-full justify-end space-x-4">
           <div className="flex items-center space-x-2 bg-gray-100 px-4 py-1 rounded-2xl shadow-md h-10">
             {/* Like Button */}
-            <button className="flex items-center text-gray-700 space-x-2">
+            <button className="flex items-center text-gray-700 space-x-2" onClick={handleIsVideoLike}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
+                fill={isVideoLike ? "currentColor" : "none"}
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
@@ -74,17 +89,17 @@ const WatchVideo = () => {
                   d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z"
                 />
               </svg>
-              <span>{getFormattedCount(statistics.likeCount)}</span>
+              <span>{numberOfLikes}</span>
             </button>
 
             {/* Divider */}
             <span className="border-l border-gray-400 h-7"></span>
 
             {/* Dislike Button */}
-            <button className="flex items-center text-gray-700 space-x-2">
+            <button className="flex items-center text-gray-700 space-x-2" onClick={handleIsVideoDislike}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
+                fill={isVideoDislike ? "currentColor" : "none"}
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
