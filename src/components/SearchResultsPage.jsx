8f5caae12b7c3aafa6114df47c5f91821d30a9d3
AbdulRehman2236 <abdulrehmanfayyaz361@gmail.com>
2024-09-18 19:34:33 +0500

    Added search list functionality

--- src/components/SearchResultsPage.jsx
+++ src/components/SearchResultsPage.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SearchVideoCard from "./SearchVideoCard";
+import { useSelector } from "react-redux";
+import { GET_SEARCH_VIDEOS_API, GET_VIDEO_LIST_BY_IDS } from "../utils/constants";
+import { useSearchParams } from "react-router-dom";
 
 const SearchResultsPage = () => {
-  return <div>SearchResultsPage</div>;
+  const [searchKeyword] = useSearchParams();
+  const [searchList, setSearchList] = useState([]);
+  const isSidebarOpen = useSelector((store) => store.sidebar.isSidebarOpen);
+
+  const getSearchList = async () => {
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const searchVideoIds = [];
+
+    const data = await fetch(GET_SEARCH_VIDEOS_API(searchKeyword.get("search_query"), API_KEY));
+    const json = await data.json();
+    json.items.map((video) => searchVideoIds.push(video.id.videoId));
+
+    const videosData = await fetch(GET_VIDEO_LIST_BY_IDS(searchVideoIds, API_KEY));
+    const videosJson = await videosData.json();
+    setSearchList(videosJson);
+    console.log(videosJson);
+  };
+
+  useEffect(() => {
+    getSearchList();
+  }, []);
+
+  if (searchList.length === 0) return;
+
+  return (
+    <div className={`${isSidebarOpen ? "col-span-10" : "col-span-12"} mt-6 px-4`}>
+      {searchList.items.map((item) => (
+        <SearchVideoCard item={item} />
+      ))}
+    </div>
+  );
 };
 
 export default SearchResultsPage;
---
+++ src/components/SearchVideoCard.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { formatDuration, getFormattedCount, timeSincePublished } from "../utils/helpers";
+
+const SearchVideoCard = ({ item }) => {
+  const { snippet, statistics, contentDetails } = item;
+  const { title, description, thumbnails, channelTitle, publishedAt } = snippet;
+  return (
+    <div className="bg-white overflow-hidden rounded-xl flex mb-4">
+      <div className="relative w-2/3">
+        <img
+          src={thumbnails?.medium?.url}
+          alt="Card1"
+          className={`w-full h-72 object-fill rounded-xl hover:cursor-pointer`}
+        />
+        <p className="absolute bottom-2 right-2 bg-black bg-opacity-60 text-white text-sm px-1 py-0.5 rounded">
+          {formatDuration(contentDetails.duration)}
+        </p>
+      </div>
+
+      <div className="px-4 py-2 w-full">
+        <div className="flex">
+          <h3 className="text-lg font-semibold w-full">{title}</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-9 pl-1 flex justify-end"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+            />
+          </svg>
+        </div>
+        <div className="flex space-x-2 pb-4">
+          <p className="text-gray-600 text-sm">{getFormattedCount(statistics.viewCount)} views . </p>
+          <p className="text-gray-600 text-sm">{timeSincePublished(publishedAt)}</p>
+        </div>
+        <div className="flex space-x-3">
+          <img
+            src="https://yt3.ggpht.com/CuOWVYxYp6iHd2QbDRnFZ81PZw7pCAwWBGU8QQfXlbIJV6RP8ZPKRLNoxKiBZm-kGtCCLaudrfI=s68-c-k-c0x00ffffff-no-rj"
+            alt="channel icon"
+            className="size-9 rounded-full mt-0.5"
+          />
+          <p className="text-slate-600 mt-2 font-semibold text-sm">{channelTitle}</p>
+        </div>
+        <p className="text-gray-600 text-sm mt-5">{description.slice(0, 115).concat("...")}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SearchVideoCard;
